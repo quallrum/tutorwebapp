d8234f64228a85860635550eabd6dac1c138e31b
Quallrum <max.kovalenko.29@gmail.com>
2020-03-15 13:02:20 +0200

    Show group

--- resources/views/group/index.blade.php
+++ resources/views/group/index.blade.php
@@ -1,6 +1,7 @@
 @if ($groups)
 	@foreach ($groups as $group)
-		<a href="{{ route('group.edit', ['group' => $group->id]) }}">{{ $group->title }}</a><br>
+		<a href="{{ route('group.show', ['group' => $group->id]) }}">{{ $group->title }}</a>
+		<a href="{{ route('group.edit', ['group' => $group->id]) }}">edit</a><br>
 	@endforeach
 @else
 No groups found
