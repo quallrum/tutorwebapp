082f690c52e3e2685657f47effd9ba4406b3312a
Quallrum <max.kovalenko.29@gmail.com>
2020-03-17 12:05:03 +0200

    Password reset pages

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,7 +1,38 @@
 @extends('layouts.app')
 
+@section('head')
+    <link rel="stylesheet" href="/css/main.min.css"/>
+@endsection
+
+@section('scripts')
+    <script src="/js/setNewPassword.js"></script>
+@endsection
+
 @section('content')
-<div class="container">
+    <section class="container setNewPassword">
+		@include('shared.alerts')
+		<div class="setNewPassword__window">
+			<h2 class="setNewPassword__heading">Установите новый пароль</h2>
+			<form class="setNewPassword__form" action="{{ route('password.update') }}" method="post" name="setNewPassword">
+				<label class="setNewPassword__item">
+					<p class="setNewPassword__text">Email</p>
+					<input class="setNewPassword__input" type="email" name="email" id="emailInput" disabled="disabled" value="{{ $email ?? old('email') }}" autocomplete="email"/>
+				</label>
+				<label class="setNewPassword__item">
+					<p class="setNewPassword__text">Новый пароль</p>
+					<input class="setNewPassword__input" type="password" name="password" id="passwordInput" autocomplete="new-password"/>
+					<p class="setNewPassword__capture" id="passwordLength">Пароль должен содержать не менее 4 символов.</p>
+				</label>
+				<label class="setNewPassword__item">
+					<p class="setNewPassword__text">Повторите новый пароль</p>
+					<input class="setNewPassword__input" type="password" name="password_confirmation" id="passwordRepeatInput" autocomplete="new-password"/>
+					<p class="setNewPassword__capture" id="passwordsAreNotTheSame">Пароли не совпадают.</p>
+				</label>
+				<button class="setNewPassword__submit" type="submit">Готово</button>
+			</form>
+		</div>
+	</section>
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -61,5 +92,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
