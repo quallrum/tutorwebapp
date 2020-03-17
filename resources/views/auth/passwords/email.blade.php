66fadaca916ad5c286ed144fa4aee710aeb68411
Quallrum <max.kovalenko.29@gmail.com>
2020-03-17 12:05:03 +0200

    Password reset pages

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,7 +1,29 @@
 @extends('layouts.app')
 
+@section('head')
+    <link rel="stylesheet" href="/css/main.min.css"/>
+@endsection
+
+@section('scripts')
+    <script src="/js/forgetPassword.js"></script>
+@endsection
+
 @section('content')
-<div class="container">
+    <section class="container forgetPassword">
+		@include('shared.errors')
+		<div class="forgetPassword__window">
+			<h2 class="forgetPassword__heading">Восстановление пароля</h2>
+			<form class="forgetPassword__form" action="{{ route('password.email') }}" method="post" name="forgetPassword">
+                @csrf
+				<label>
+					<p class="forgetPassword__text">Email</p>
+					<input class="forgetPassword__input" type="email" name="email" autocomplete="email" id="forgetPasswordInput"/>
+				</label>
+				<button class="forgetPassword__submit" type="submit">Готово</button>
+			</form>
+		</div>
+	</section>
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -43,5 +65,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,7 +1,33 @@
 @extends('layouts.app')
 
+@section('head')
+    <link rel="stylesheet" href="/css/main.min.css"/>
+@endsection
+
+@section('scripts')
+    <script src="/js/emailVerification.js"></script>
+@endsection
+
 @section('content')
-<div class="container">
+	<section class="container emailVerification">
+		@include('shared.alerts')
+		<div class="emailVerification__window">
+			<h2 class="emailVerification__heading">Подтверждение Email</h2>
+			<div class="emailVerification__textBlock">
+				<div class="emailVerification__alertInText">
+					<p class="emailVerification__alertInText-text emailVerification__alertInText-text--bold">Ваш Email не подтвержен!</p>
+					<p class="emailVerification__alertInText-text">Пожалуйста, подтвердите свой Email.</p>
+				</div>
+				<p class="emailVerification__text">Мы отправили письмо для верификации на <a href="mailto:{{ $user->email }}">{{ $user->email }}</a>. Если вы не можете найти письмо для верификации, пожалуйста, проверьте корзину и спам.</p>
+				<p class="emailVerification__text">Если вы не получили >письмо для верификации, пожалуйста, нажмите на кнопку "Отправить снова" ниже.</p>
+			</div>
+            <form action="{{ route('verification.resend') }}" method="post" name="sendVerificationEmailAgain">
+                @csrf
+				<button class="emailVerification__button" type="submit">Отправить снова</button>
+			</form>
+		</div>
+	</section>
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -24,5 +50,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
