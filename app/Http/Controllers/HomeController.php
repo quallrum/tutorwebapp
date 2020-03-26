8ed7127f7e5b554012fde2674a63cb39c4027c60
Quallrum <max.kovalenko.29@gmail.com>
2020-03-26 15:52:22 +0200

    Selecting subject for monitor

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Group;
 
 class HomeController extends Controller{
 
@@ -16,6 +18,10 @@ class HomeController extends Controller{
         $user = Auth::user();
 
         if($user->role->name == 'tutor') return redirect()->route('journal.group');
+        if($user->role->name == 'monitor'){
+            $group = Group::where('monitor_id', $user->id)->pluck('id')->first();
+            return redirect()->route('journal.subject', ['group' => $group]);
+        }
 
         return view('home');
     }
