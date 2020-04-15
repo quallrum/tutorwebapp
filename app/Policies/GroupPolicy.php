37b734e6205b6bacf5477f3ede9632b4f8be5011
Quallrum <max.kovalenko.29@gmail.com>
2020-04-15 22:43:23 +0300

    Group policy and passing additional agrs to policy gate wrapper

---
+++ app/Policies/GroupPolicy.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Group;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class GroupPolicy{
+    use HandlesAuthorization;
+
+    public function before($user){
+        if($user->role->name == 'admin') return true;
+	}
+	
+	public function list($user){
+		return false;
+	}
+
+	public function view($user){
+		return false;
+	}
+
+	public function create($user){
+		return false;
+	}
+
+    public function edit($user, Group $group){
+        return $user->role->name == 'monitor' and $user->id == $group->monitor->id;
+	}
+	
+	public function monitor($user){
+		return false;
+	}
+
+}
