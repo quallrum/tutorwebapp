04102a3324533a21187bed2b6ec31c5140ed4372
Quallrum <max.kovalenko.29@gmail.com>
2020-04-27 14:30:42 +0300

    Russian language

---
+++ resources/lang/ru/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Ваш пароль был сброшен',
+    'sent' => 'Ссылка была отправлена на указанный e-mail адрес',
+    'throttled' => 'Пожалуйста, подождите прежде чем продолжить',
+    'token' => 'Неправильный токен для восстановления пароля',
+    'user' => "Пользователь с таким e-mail адресом не найден",
+
+];
