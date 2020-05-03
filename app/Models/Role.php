97c2aca31e3165e48ef5c6f71cf7b2e9ece7fbf5
quallrum <max.kovalenko.29@gmail.com>
2020-05-03 19:25:57 +0300

    Role -> id map as class constants

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,5 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model{
+
+    // Role -> id map
+    public const admin      = 1;
+    public const user       = 2;
+    public const tutor      = 3;
+    public const monitor    = 4;
+    public const group      = 5;
+    
     public function users(){ return $this->hasMany(User::class); }
 }
