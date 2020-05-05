692c7bafb2edbc70a433f686065f0a993d9cf81f
quallrum <max.kovalenko.29@gmail.com>
2020-05-05 19:18:15 +0300

    Add handling of absence of sections on home page

--- public/js/home.js
+++ public/js/home.js
@@ -1,14 +1,16 @@
 'use strict';
+try {
+    document.getElementById('editEmailCross').addEventListener('click', hideEditEmailSection);
+    document.getElementById('editEmailButton').addEventListener('click', showEditEmailSection);
 
-document.getElementById('editEmailCross').addEventListener('click', hideEditEmailSection);
-document.getElementById('editEmailButton').addEventListener('click', showEditEmailSection);
+    document.getElementById('editPasswordCross').addEventListener('click', hideEditPasswordSection);
+    document.getElementById('editPasswordButton').addEventListener('click', showEditPasswordSection);
 
-document.getElementById('editPasswordCross').addEventListener('click', hideEditPasswordSection);
-document.getElementById('editPasswordButton').addEventListener('click', showEditPasswordSection);
-
-document.getElementById('editTelegramCross').addEventListener('click', hideEditTelegramSection);
-document.getElementById('editTelegramButton').addEventListener('click', showEditTelegramSection);
-
+    document.getElementById('editTelegramCross').addEventListener('click', hideEditTelegramSection);
+    document.getElementById('editTelegramButton').addEventListener('click', showEditTelegramSection);
+} catch (e) {
+    console.log(e);
+}
 
 function hideEditEmailSection() {
     document.getElementById('editEmailSection').style.display = 'none';
@@ -40,6 +42,7 @@ function checkEscAndHideWindow(e) {
     if (e.keyCode === 27) {
         hideEditEmailSection();
         hideEditPasswordSection();
+        hideEditTelegramSection();
     }
 }
 
@@ -86,116 +89,118 @@ try {
             xhr.send(formData);
         }
     });
+
+
+
+
+    let formEditEmail = document.forms['editEmail'];
+    formEditEmail.addEventListener('submit', function (e) {
+        e.preventDefault();
+        let email = document.getElementById('editEmailInput').value;
+        if (checkEmail(email)) {
+            sendAjaxEditEmail();
+        }
+    });
+
+    function sendAjaxEditEmail() {
+        let formData = new FormData(formEditEmail);
+        let action = formEditEmail.getAttribute('action');
+        let xhr = new XMLHttpRequest();
+
+        try {
+
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    hideEditEmailSection();
+                    if (xhr.status == 200) {
+                        putTextInSuccessAlertAndShowIt('Данные успешно обновлены');
+                        document.getElementById('emailText').innerText = formData.get('email');
+                    } else {
+                        try {
+                            let arrayJSON = JSON.parse(xhr.responseText);
+                            let errors = arrayJSON.errors;
+                            if (errors) {
+                                let strWithError = '';
+                                for (let error in errors) {
+                                    strWithError += error + '\n';
+                                }
+                                putTextInAlertAndShowIt(strWithError);
+                            } else {
+                                putTextInAlertAndShowIt('Упс, что-то пошло не так(');
+                            }
+                        } catch (e) {
+                            putTextInAlertAndShowIt('Упс, что-то пошло не так(');
+                        }
+
+                    }
+                }
+            }
+
+            xhr.open('POST', action);
+            xhr.setRequestHeader('Accept', 'application/json');
+            xhr.send(formData);
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+
+    let formEditPassword = document.forms['editPassword'];
+    formEditPassword.addEventListener('submit', function (e) {
+        e.preventDefault();
+        if (checkAllInputs()) {
+            sendAjaxEditPassword();
+        }
+    });
+
+    function sendAjaxEditPassword() {
+        let formData = new FormData(formEditPassword);
+        let action = formEditPassword.getAttribute('action');
+        let xhr = new XMLHttpRequest();
+
+        try {
+
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    hideEditPasswordSection();
+                    // clear password inputs
+                    document.getElementById('passwordInput').value = '';
+                    document.getElementById('passwordRepeatInput').value = '';
+                    if (xhr.status == 200) {
+                        putTextInSuccessAlertAndShowIt('Данные успешно обновлены');
+                    } else {
+                        try {
+                            let arrayJSON = JSON.parse(xhr.responseText);
+                            let errors = arrayJSON.errors;
+                            if (errors) {
+                                let strWithError = '';
+                                for (let error in errors) {
+                                    strWithError += error + '\n';
+                                }
+                                putTextInAlertAndShowIt(strWithError);
+                            } else {
+                                putTextInAlertAndShowIt('Упс, что-то пошло не так(');
+                            }
+                        } catch (e) {
+                            putTextInAlertAndShowIt('Упс, что-то пошло не так(');
+                        }
+
+                    }
+                }
+            }
+
+            xhr.open('POST', action);
+            xhr.setRequestHeader('Accept', 'application/json');
+            xhr.send(formData);
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
 } catch (e) {
 
-}
-
-
-
-let formEditEmail = document.forms['editEmail'];
-formEditEmail.addEventListener('submit', function (e) {
-    e.preventDefault();
-    let email = document.getElementById('editEmailInput').value;
-    if (checkEmail(email)) {
-        sendAjaxEditEmail();
-    }
-});
-
-function sendAjaxEditEmail() {
-    let formData = new FormData(formEditEmail);
-    let action = formEditEmail.getAttribute('action');
-    let xhr = new XMLHttpRequest();
-
-    try {
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                hideEditEmailSection();
-                if (xhr.status == 200) {
-                    putTextInSuccessAlertAndShowIt('Данные успешно обновлены');
-                    document.getElementById('emailText').innerText = formData.get('email');
-                } else {
-                    try {
-                        let arrayJSON = JSON.parse(xhr.responseText);
-                        let errors = arrayJSON.errors;
-                        if (errors) {
-                            let strWithError = '';
-                            for (let error in errors) {
-                                strWithError += error + '\n';
-                            }
-                            putTextInAlertAndShowIt(strWithError);
-                        } else {
-                            putTextInAlertAndShowIt('Упс, что-то пошло не так(');
-                        }
-                    } catch (e) {
-                        putTextInAlertAndShowIt('Упс, что-то пошло не так(');
-                    }
-
-                }
-            }
-        }
-
-        xhr.open('POST', action);
-        xhr.setRequestHeader('Accept', 'application/json');
-        xhr.send(formData);
-
-    } catch (e) {
-        console.log(e);
-    }
-}
-
-
-let formEditPassword = document.forms['editPassword'];
-formEditPassword.addEventListener('submit', function (e) {
-    e.preventDefault();
-    if (checkAllInputs()) {
-        sendAjaxEditPassword();
-    }
-});
-
-function sendAjaxEditPassword() {
-    let formData = new FormData(formEditPassword);
-    let action = formEditPassword.getAttribute('action');
-    let xhr = new XMLHttpRequest();
-
-    try {
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                hideEditPasswordSection();
-                // clear password inputs
-                document.getElementById('passwordInput').value = '';
-                document.getElementById('passwordRepeatInput').value = '';
-                if (xhr.status == 200) {
-                    putTextInSuccessAlertAndShowIt('Данные успешно обновлены');
-                } else {
-                    try {
-                        let arrayJSON = JSON.parse(xhr.responseText);
-                        let errors = arrayJSON.errors;
-                        if (errors) {
-                            let strWithError = '';
-                            for (let error in errors) {
-                                strWithError += error + '\n';
-                            }
-                            putTextInAlertAndShowIt(strWithError);
-                        } else {
-                            putTextInAlertAndShowIt('Упс, что-то пошло не так(');
-                        }
-                    } catch (e) {
-                        putTextInAlertAndShowIt('Упс, что-то пошло не так(');
-                    }
-
-                }
-            }
-        }
-
-        xhr.open('POST', action);
-        xhr.setRequestHeader('Accept', 'application/json');
-        xhr.send(formData);
-
-    } catch (e) {
-        console.log(e);
-    }
 }
 
 function checkEmail(str) {
@@ -216,59 +221,62 @@ function checkPassword(str) {
     }
 }
 
+try {
 
-document.getElementById('editEmailInput').addEventListener('input', function () {
-    let value = this.value;
-    let button = document.querySelector('.editEmail__submit');
-    if (checkEmail(value)) {
-        button.classList.add('editEmail__submit--active');
-    } else {
-        button.classList.remove('editEmail__submit--active');
-    }
-});
+    document.getElementById('editEmailInput').addEventListener('input', function () {
+        let value = this.value;
+        let button = document.querySelector('.editEmail__submit');
+        if (checkEmail(value)) {
+            button.classList.add('editEmail__submit--active');
+        } else {
+            button.classList.remove('editEmail__submit--active');
+        }
+    });
 
-document.getElementById('passwordInput').addEventListener('input', function () {
-    let passwordValue = this.value;
-    let capture = document.getElementById('passwordLength');
-    if (checkPassword(passwordValue)) {
-        capture.style.visibility = "hidden";
-    } else {
-        capture.style.visibility = 'visible';
-        document.querySelector('.editPassword__submit').classList.remove('editPassword__submit--active');
-    }
+    document.getElementById('passwordInput').addEventListener('input', function () {
+        let passwordValue = this.value;
+        let capture = document.getElementById('passwordLength');
+        if (checkPassword(passwordValue)) {
+            capture.style.visibility = "hidden";
+        } else {
+            capture.style.visibility = 'visible';
+            document.querySelector('.editPassword__submit').classList.remove('editPassword__submit--active');
+        }
 
-    if (checkAllInputs()) {
-        allDataIsValid();
-    }
-});
+        if (checkAllInputs()) {
+            allDataIsValid();
+        }
+    });
 
-document.getElementById('passwordRepeatInput').addEventListener('input', function () {
-    let passwordRepeat = this.value;
-    let password = document.getElementById('passwordInput').value;
-    let capture = document.getElementById('passwordsAreNotTheSame');
+    document.getElementById('passwordRepeatInput').addEventListener('input', function () {
+        let passwordRepeat = this.value;
+        let password = document.getElementById('passwordInput').value;
+        let capture = document.getElementById('passwordsAreNotTheSame');
 
-    if (passwordRepeat === password) {
-        capture.style.visibility = "hidden";
-    } else {
-        capture.style.visibility = 'visible';
-        document.querySelector('.editPassword__submit').classList.remove('editPassword__submit--active');
-    }
+        if (passwordRepeat === password) {
+            capture.style.visibility = "hidden";
+        } else {
+            capture.style.visibility = 'visible';
+            document.querySelector('.editPassword__submit').classList.remove('editPassword__submit--active');
+        }
 
-    if (checkAllInputs()) {
-        allDataIsValid();
-    }
-});
+        if (checkAllInputs()) {
+            allDataIsValid();
+        }
+    });
 
-document.getElementById('editTelegramInput').addEventListener('input', function () {
-    let value = this.value;
-    let button = document.querySelector('.editTelegram__submit');
-    if (value != "") {
-        button.classList.add('editTelegram__submit--active');
-    } else {
-        button.classList.remove('editTelegram__submit--active');
-    }
-});
-
+    document.getElementById('editTelegramInput').addEventListener('input', function () {
+        let value = this.value;
+        let button = document.querySelector('.editTelegram__submit');
+        if (value != "") {
+            button.classList.add('editTelegram__submit--active');
+        } else {
+            button.classList.remove('editTelegram__submit--active');
+        }
+    });
+} catch (e) {
+    console.log(e);
+}
 function checkAllInputs() {
     let password = document.getElementById('passwordInput').value;
     let passwordRepeat = document.getElementById('passwordRepeatInput').value;
@@ -287,51 +295,54 @@ function allDataIsValid() {
 
 // for fullname
 
-let fullnameForm = document.forms['homeFullname'];
-fullnameForm.addEventListener('submit', function (e) {
-    e.preventDefault();
+try {
+    let fullnameForm = document.forms['homeFullname'];
+    fullnameForm.addEventListener('submit', function (e) {
+        e.preventDefault();
 
-    if (checkFullnameInputs()) {
-        let formData = new FormData(fullnameForm);
-        let action = fullnameForm.getAttribute('action');
-        let xhr = new XMLHttpRequest();
+        if (checkFullnameInputs()) {
+            let formData = new FormData(fullnameForm);
+            let action = fullnameForm.getAttribute('action');
+            let xhr = new XMLHttpRequest();
 
-        try {
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === 4) {
-                    if (xhr.status == 200) {
-                        putTextInSuccessAlertAndShowIt('Данные успешно обновлены');
-                    } else {
-                        try {
-                            let arrayJSON = JSON.parse(xhr.responseText);
-                            let errors = arrayJSON.errors;
+            try {
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4) {
+                        if (xhr.status == 200) {
+                            putTextInSuccessAlertAndShowIt('Данные успешно обновлены');
+                        } else {
+                            try {
+                                let arrayJSON = JSON.parse(xhr.responseText);
+                                let errors = arrayJSON.errors;
 
-                            let strWithError = '';
-                            for (let error in errors) {
-                                strWithError += error + '\n';
+                                let strWithError = '';
+                                for (let error in errors) {
+                                    strWithError += error + '\n';
+                                }
+                                putTextInAlertAndShowIt(strWithError);
+                            } catch (e) {
+                                putTextInAlertAndShowIt('Упс, что-то пошло не так(');
                             }
-                            putTextInAlertAndShowIt(strWithError);
-                        } catch (e) {
-                            putTextInAlertAndShowIt('Упс, что-то пошло не так(');
                         }
                     }
                 }
+
+                xhr.open('POST', action);
+                xhr.setRequestHeader('Accept', 'application/json');
+                xhr.send(formData);
+
+            } catch (e) {
+                console.log(e);
             }
-
-            xhr.open('POST', action);
-            xhr.setRequestHeader('Accept', 'application/json');
-            xhr.send(formData);
-
-        } catch (e) {
-            console.log(e);
+        } else {
+            putTextInAlertAndShowIt('ФИО не дожно быть пустым');
         }
-    } else {
-        putTextInAlertAndShowIt('ФИО не дожно быть пустым');
-    }
 
 
-});
-
+    });
+} catch (e) {
+    console.log(e);
+}
 function checkFullnameInputs() {
     let lastname = document.getElementById('lastname').value;
     let firstname = document.getElementById('firstname').value;
