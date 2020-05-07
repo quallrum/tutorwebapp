2054595a007d22986c9ccfda4775af2d186f41bf
quallrum <max.kovalenko.29@gmail.com>
2020-05-07 15:25:51 +0300

    Backend for marks updating

--- app/Http/Controllers/MarkController.php
+++ app/Http/Controllers/MarkController.php
@@ -73,9 +73,10 @@ class MarkController extends Controller{
 		$this->authorize('mark.edit');
 
 		$request->validate([
-			'journal'		=> ['nullable', 'array'],
+			'header'		=> ['nullable', 'array'],
+			'mark'			=> ['nullable', 'array'],
 			'new_header'	=> ['nullable', 'array'],
-			'new_journal'	=> ['nullable', 'array'],
+			'new_mark'		=> ['nullable', 'array'],
 			'delete'		=> ['nullable', 'array'],
 		]);
 
@@ -84,11 +85,32 @@ class MarkController extends Controller{
 		$failed_delete = [];
 		$errors = [];
 
-		if($request->input('journal')){
-			foreach ($request->input('journal') as $id => $value) {
-				$record = Journal::find($id);
-				if($record and ($record->editable() or Auth::user()->role->name == 'admin') and $record->subject_id == $subject->id){
+		if($request->has('header')){
+			foreach ($request->input('header') as $id => $title) {
+				$column = MarkColumn::find($id);
+
+				if($column and $column->subject_id == $subject->id){
+					$column->title = $title;
+				
+					if(!$column->save()){
+						$failed[] = $id;
+						Log::error('User '.Auth::user()->id.' failed to update column '.$id.' due to unexpected error');
+					}
+				}
+				else{
+					$failed[] = $id;
+					Log::warning('User '.Auth::user()->id.' failed to update column '.$id.': column doesn\'t belong to subject '.$subject->id.' or column not found');
+				}
+			}
+		}
+
+		if($request->has('mark')){
+			foreach ($request->input('mark') as $id => $value) {
+				$record = Mark::find($id);
+
+				if($record and $record->column->subject_id == $subject->id){
 					$record->value = $value;
+				
 					if(!$record->save()){
 						$failed[] = $id;
 						Log::error('User '.Auth::user()->id.' failed to update record '.$id.' due to unexpected error');
@@ -96,55 +118,52 @@ class MarkController extends Controller{
 				}
 				else{
 					$failed[] = $id;
-					Log::warning('User '.Auth::user()->id.' failed to update record '.$id.': record not editable, record doesn\'t belong to subject '.$subject->id.' or record not found');
+					Log::warning('User '.Auth::user()->id.' failed to update record '.$id.': record doesn\'t belong to subject '.$subject->id.' or record not found');
 				}
 			}
 		}
 		
-		if($request->input('new_journal')){
-			$today = (new \Datetime)->format('Y-m-d');
-			$last = Journal::lastDate($group->students()->first(), $subject);
+		if($request->has('new_header') and $request->has('new_mark')){
+			foreach ($request->input('new_header') as $new_column) {
+				$column = new MarkColumn;
+				$column->subject_id = $subject->id;
+				$column->group_id = $group->id;
 
-			if($today != $last or Auth::user()->role->name == 'admin'){
-				foreach ($request->input('new_journal') as $column) {
-					foreach ($column as $id => $value) {
-						if($group->hasStudent($id)){
-							$record = new Journal;
-							$record->fill([
-								'student_id'	=> $id,
-								'subject_id'	=> $subject->id,
-							]);
-							$record->value = $value;
-							if(!$record->save()){
-								$failed_new[] = $id;
-								Log::error('User '.Auth::user()->id.' failed to create record for student '.$id.' due to unexpected error');
-							}
-						}
-						else{
-							Log::warning('User '.Auth::user()->id.' failed to create record for student '.$id.': student doesn\'t belong to group '.$group->id);
-						}
-					}	
+				if(!$column->save() and !$column->save()){
+					Log::warning('User '.Auth::user()->id.' failed to create new column due to unexpected error');
+					$failed_new[] = 'column';
+					continue;
 				}
-			}
-			else{
-				$errors[] = 'You can add only one column per day';
-				Log::warning('User '.Auth::user()->id.' tried to add more than one column: today is '.$today.', last column with date '.$last);
+				
+				foreach ($new_column as $id => $value) {
+					if($group->hasStudent($id)){
+						$record = new Mark;
+						$record->column_id = $column->id;
+						$record->student_id	= $id;
+						$record->value = $value;
+
+						if(!$record->save() and !$record->save()){
+							$failed_new[] = $id;
+							Log::error('User '.Auth::user()->id.' failed to create record for student '.$id.' due to unexpected error');
+						}
+					}
+					else{
+						Log::warning('User '.Auth::user()->id.' failed to create record for student '.$id.': student doesn\'t belong to group '.$group->id);
+					}
+				}	
 			}
 		}
 
 		if($request->has('delete')){
-			foreach ($request->input('delete') as $id) {
-				$record = Journal::find($id);
-				if($record 
-					and ($record->editable() or Auth::user()->role->name == 'admin') 
-					and $record->subject_id == $subject->id 
-					and $record->delete()
-				){
-					Log::notice('User'.Auth::user()->id.' deleted record '.$id.'. Soft delete was used');
+			foreach ($request->input('delete') as $column) {
+				$column = MarkColumn::find($id);
+				if($column and $column->column->subject_id == $subject->id){
+					if($column->records()->delete() and $column->delete()) Log::notice('User'.Auth::user()->id.' deleted columnd '.$id.'. Soft delete was used');
+					else Log::error('User '.Auth::user()->id.' failed to delete column '.$id.' due to unexpected error');
 				}
 				else{
 					$failed_delete[] = $id;
-					Log::warning('User '.Auth::user()->id.' failed to delete record '.$id.': record not editable, record doesn\'t belong to subject '.$subject->id.', record not found or deleting failed.');
+					Log::warning('User '.Auth::user()->id.' failed to delete column '.$id.':  record doesn\'t belong to subject '.$subject->id.', record not found or deleting failed.');
 				}
 			}
 		}
@@ -152,13 +171,8 @@ class MarkController extends Controller{
 		if($failed or $failed_new or $failed_delete) $errors[] = 'Not all records was updated, contact admin';
 
 		if($request->wantsJson()){
-			if($errors) return response()->json([
-				'message'	=> $errors,
-			], 500);
-
-			return response()->json([
-				'message'	=> 'Journal updated!'
-			]);
+			if($errors) return response()->json(['message'	=> $errors], 500);
+			return response()->json(['message'	=> 'Journal updated!']);
 		}
 
 		if($errors) return back()->withErrors($errors);
--- app/Models/Mark/MarkColumn.php
+++ app/Models/Mark/MarkColumn.php
@@ -3,10 +3,15 @@
 namespace App\Models\Mark;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Subject;
 use App\Models\Group;
 
 class MarkColumn extends Model{
+
+	use SoftDeletes;
+
 	public function subject(){ return $this->belongsTo(Subject::class); }
 	public function group(){ return $this->belongsTo(Group::class); }
+	public function records(){ return $this->hasMany(Mark::class); }
 }
