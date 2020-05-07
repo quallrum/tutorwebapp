f5c8a6149e48ab61650953975b5b6de2633690a4
quallrum <max.kovalenko.29@gmail.com>
2020-05-07 13:11:07 +0300

    Check if subject hasMarks, scope for get subjects with marks

--- app/Models/SubjectType.php
+++ app/Models/SubjectType.php
@@ -5,5 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class SubjectType extends Model{
+
+	public static $hasMarks = [2, 3, 4];
+
+	public static function hasMarks($type){
+		$type = $type instanceof SubjectType ? $type->id : $type;
+		return \in_array($type, static::$hasMarks);
+	}
+
 	public function subjects(){ return $this->hasMany(Subject::class, 'type_id'); }
 }
